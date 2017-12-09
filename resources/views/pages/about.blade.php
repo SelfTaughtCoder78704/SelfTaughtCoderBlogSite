fac07ffff13f175b2873b3ee986812dbc20c2a2f
Bobby Nicholson <bobbynicholson78704@gmail.com>
2017-12-09 17:09:03 -0600

    Add files via upload

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+       <h1>{{$title}}</h1>
+       <p>Do you want to leave poverty behind?
+       You Do? Great! That's what the Self-Taught
+       Coder is all about. This is where <mark><strong>YOU</strong></mark> can make
+       a <mark><strong>SUPER-CHANGE</strong></mark>. Not just a change in your knowledge
+       of coding, but also a change in the knowledge of Living!<br>
+       Here, we want money falling from the ceiling! Here, we want to ONE DAY work from HOME! 
+       And what is wrong with that? Personally, I've done boring, go-nowhere hard labor work 
+       (out in the heat and the freezing cold). 
+       And truthfully, that shit ain't  for me! Sorry but NOT SORRY.<br>
+       And another hard and cold truth...<br>
+       This code shit can MAKE YOUR FUCKIN DREAMS COME TRUE! But nobody wants you to know that.
+       Sure, you know and they say you can make a lot of money IF you're smart enough. But they 
+       LIE! This Code Shit Isn't That Hard To Learn. And you don't have to be a genius. All you need is 
+       PERSISTANCE, a computer, and the drive in your heart to break away from that Hard-taught Old Life. </p>
+@endsection
